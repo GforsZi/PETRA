54402de444cf34a039e1b379ed31c8eef5691d8f
nurmieai <nurmieai@gmail.com>
2025-09-27 13:31:03 +0700

    adding transaction/add

--- resources/views/user/book/search.blade.php
+++ resources/views/user/book/search.blade.php
@@ -2,14 +2,14 @@
     <x-slot:title>{{ $title }}</x-slot:title>
 
   <div class="input-group mb-3 shadow-sm border border-dark rounded">
-  <input 
-    type="text" 
-    class="form-control border-0" 
-    placeholder="Cari sesuatu..." 
+  <input
+    type="text"
+    class="form-control border-0"
+    placeholder="Cari sesuatu..."
     aria-label="Search"
   >
-  <button 
-    class="btn btn-primary border-0 px-4 d-flex align-items-center justify-content-center" 
+  <button
+    class="btn btn-primary border-0 px-4 d-flex align-items-center justify-content-center"
     type="button"
   >
     <i class="bi bi-search fs-5"></i>
@@ -18,45 +18,45 @@
 
 <div class="container mt-3">
   <div class="border border-dark rounded p-2">
-    
-    
+
+
     <h5 class="mb-2"><b>Buku Terpopuler</b></h5>
 
-   
+
     <div class="overflow-auto">
       <div class="row flex-nowrap gx-1">
-        
-       
+
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 1">
         </div>
 
-      
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 2">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 3">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 4">
         </div>
 
-       
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 5">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 6">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 7">
         </div>
@@ -85,45 +85,45 @@
 
 <div class="container mt-3">
   <div class="border border-dark rounded p-2">
-    
-    
+
+
     <h5 class="mb-2"><b>Buku Terbaru</b></h5>
 
-   
+
     <div class="overflow-auto">
       <div class="row flex-nowrap gx-1">
-        
-      
+
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 1">
         </div>
 
-      
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 2">
         </div>
 
-      
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 3">
         </div>
 
-    
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 4">
         </div>
 
-   
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 5">
         </div>
 
-       
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 6">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 7">
         </div>
@@ -152,45 +152,45 @@
 
 <div class="container mt-3">
   <div class="border border-dark  rounded p-2">
-    
-    
+
+
     <h5 class="mb-2"><b>Buku Pelajaran</b></h5>
 
-   
+
     <div class="overflow-auto">
       <div class="row flex-nowrap gx-1">
-        
-       
+
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 1">
         </div>
 
-       
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 2">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 3">
         </div>
 
-       
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 4">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 5">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 6">
         </div>
 
-      
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 7">
         </div>
@@ -219,45 +219,45 @@
 
 <div class="container mt-3">
   <div class="border border-dark rounded p-2">
-    
-    
+
+
     <h5 class="mb-2"><b>Buku Novel</b></h5>
 
-   
+
     <div class="overflow-auto">
       <div class="row flex-nowrap gx-1">
-        
-       
+
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 1">
         </div>
 
-  
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 2">
         </div>
 
-      
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 3">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 4">
         </div>
 
-      
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 5">
         </div>
 
-        
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 6">
         </div>
 
-      
+
         <div class="col-2 col-lg-1-7">
           <img src="{{ asset('logo/landing/Aesop.png') }}" class="img-fluid rounded book-img" alt="Buku 7">
         </div>
@@ -284,7 +284,7 @@
 </div>
 
 <style>
-  
+
   @media (min-width: 992px) {
     .col-lg-1-7 {
       flex: 0 0 calc(100% / 7);
@@ -300,14 +300,14 @@
     }
   }
 
-  
+
   .book-img {
     width: 100%;
-    height: 160px;   
+    height: 160px;
     object-fit: cover;
   }
 
-  
+
   .overflow-auto::-webkit-scrollbar {
     display: none;
   }
