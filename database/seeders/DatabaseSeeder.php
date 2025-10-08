d805b59487289adbfd09221003460a247da94fbf
nurmieai <nurmieai@gmail.com>
2025-10-08 10:44:03 +0700

    update kren

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
@@ -17,36 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create();
-        // User::factory(10)->create();
-
-        $admin_role = Role::firstOrCreate([
-            'rl_name' => 'Pustakawan',
-            'rl_description' => 'sarana perasarana',
-            'rl_admin' => true,
-        ]);
-        $user_role = Role::firstOrCreate([
-            'rl_name' => 'Siswa',
-            'rl_description' => 'user role',
-        ]);
-
-        $admin = User::firstOrCreate(
-            ['usr_no_wa' => '08223456789'],
-            [
-                'name' => 'gfors',
-                'password' => Hash::make('12345'),
-                'usr_activation' => true,
-                'usr_role_id' => $admin_role->rl_id,
-            ],
-        );
-        $user = User::firstOrCreate(
-            ['usr_no_wa' => '08123456789'],
-            [
-                'name' => 'zi',
-                'password' => Hash::make('12345'),
-                'usr_activation' => true,
-                'usr_role_id' => $user_role->rl_id,
-            ],
-        );
+        $this->call([
+        UserLoginSeeder::class,
+    ]);
     }
 }
