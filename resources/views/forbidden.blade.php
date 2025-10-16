07a531d73e308809a38be22a46cde2dc42de4aec
nurmieai <nurmieai@gmail.com>
2025-10-16 13:09:18 +0700

    minor change

--- resources/views/forbidden.blade.php
+++ resources/views/forbidden.blade.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/adminlte.min.css') }}">
     <x-navbar></x-navbar>
 
-    <main class="app-main bg-body" style="height: 83.2vh;">
+    <main class="app-main bg-body" style="height: 100vh;">
         <!--begin::App Content Header-->
         <div class="app-content-header">
             <div class="container-fluid">
@@ -20,15 +20,15 @@
         <div class="d-flex flex-column justify-content-center align-items-center text-center h-100">
             <h1 class="display-1 fw-bold text-neon" style="color:#1800ecff; text-shadow:0 0 20px #1800ecff;">403</h1>
             <br>
-            <h4 class="fw-semibold mb-3 text-body-emphasis">🚫 Oops! Sepertinya akun kamu belum siap mengakses halaman ini.</h4>
+            <h4 class="fw-semibold mb-3 text-body-emphasis">🚫 Oops! Sepertinya akun kamu belum siap mengakses PETRA</h4>
             <p class="text-body-secondary">
                 Admin mungkin belum memberikan hak akses atau aktivasi akunmu masih dalam proses.<br>
                 Silakan hubungi admin untuk memastikan status akunmu.
             </p>
 
             <div class="mt-4 d-flex gap-2">
-                <a href="{{ url()->previous() }}" class="btn btn-outline-secondary">
-                    <i class="bi bi-arrow-left me-1"></i> Go Back
+                <a href="/" class="btn btn-outline-secondary">
+                    <i class="bi bi-arrow-left me-1"></i> Kembali
                 </a>
             </div>
         </div>
